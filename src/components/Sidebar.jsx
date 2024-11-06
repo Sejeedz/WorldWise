87c3ff14c06131778866f7b866a9d966c4033f49
Sejeedz <m.sajid3421@gmail.com>
2024-11-07 02:36:08 +0500

    Created

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import styles from "./Sidebar.module.css";
+import Logo from "./Logo.jsx";
+import AppNav from "./AppNav.jsx";
+import Footer from "./Footer.jsx";
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+}
+
+export default Sidebar;
